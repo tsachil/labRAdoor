2877006492e0c7e3ab477d78b9df91361126f230
tsachi <tsachil@users.noreply.github.com>
2017-11-22 07:36:12 +0200

    created
    removed particle device id and authorization key

---
+++ lab.py
@@ -0,0 +1,134 @@
+from __future__ import print_function
+import urllib2
+
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': "SessionSpeechlet - open door",
+            'content': "SessionSpeechlet - open door"
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+
+# --------------- Functions that control the skill's behavior ------------------
+
+def open_door():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+    call_rest_service()
+    
+    session_attributes = {}
+    speech_output = "Open Sesame."
+    should_end_session = True
+    return build_response(session_attributes, build_speechlet_response(
+        speech_output, None, should_end_session))
+
+
+def call_rest_service():
+    path = 'https://api.particle.io/v1/devices/XXXXXXXXXXXXXXXXXXX/door'
+    data = '{ "arg": "press" }'
+    headers = {'Authorization':'Bearer XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+                'Content-Type':'application/json'}
+    request = urllib2.Request(path, data, headers)
+    response = urllib2.urlopen(request).read()
+
+    print(response)
+
+# --------------- Events ------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return open_door()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+    
+    return open_door()
+
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
+# ---------------------------
+
